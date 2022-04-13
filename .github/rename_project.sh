d145298ffefb966e2f6d101577ef4310e0e54b41
ahcoronurb <ahcoronurb@users.noreply.github.com>
2022-04-13 12:14:09 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ahcoronurb"
+original_name="beerlog"
+original_urlname="beerlog"
+original_description="Awesome beerlog created by ahcoronurb"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv beerlog $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
